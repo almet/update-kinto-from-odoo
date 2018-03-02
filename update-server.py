34e8ecd863f5a2b2bb2de4a4b73d3e199d5edd25
Alexis Métaireau <alexis@notmyidea.org>
2018-03-02 21:22:47 +0100

    Add a functional script

---
+++ update-server.py
@@ -0,0 +1,24 @@
+import requests
+import xmlrpclib
+import sys
+
+record_url = sys.argv[1]
+kinto_user = 'vieuxsinge'
+kinto_password = sys.argv[2]
+
+odoo_url = sys.argv[3]
+odoo_username = 'contact@vieuxsinge.com'
+odoo_password = sys.argv[4]
+odoo_db = 'brasserieduvieuxsinge'
+
+from pdb import set_trace; set_trace()
+common = xmlrpclib.ServerProxy('{}/xmlrpc/2/common'.format(odoo_url))
+models = xmlrpclib.ServerProxy('{}/xmlrpc/2/object'.format(odoo_url))
+uid = common.authenticate(odoo_db, odoo_username, odoo_password, {})
+ids = models.execute_kw(odoo_db, uid, odoo_password, 'sale.order', 'search', [[['state', '=', 'sale']]])
+product_ids = [p['product_id'][0] for p in models.execute_kw(odoo_db, uid, odoo_password, 'sale.order', 'read', [ids], {'fields': ['product_id']})]
+
+volumes = {	12: 4, 13: 4.5, 14: 18, 15: 16, 16: 9, 17: 8}
+new_volume = sum([volumes[p] for p in product_ids])
+
+resp = requests.put(record_url, json={'data': {'liters': new_volume}}, auth=(kinto_user, kinto_password))
